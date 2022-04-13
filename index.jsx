ad4544005819d2dd30f7425c0973f103c3e00294
Dubuc Valentin <v.dre@laposte.net>
2022-04-13 02:04:17 +0200

    Update chart from index

--- index.jsx
+++ index.jsx
@@ -12,6 +12,13 @@ const sumWithInitial = array1.reduce((prevValue, currValue) => prevValue + currV
 Condition Ternaire : condition ? exprSiVrai : exprSiFaux
 */
 
+
+// Déclaration de nos variables/tableaux de données
+var jsonResponse;
+jsonResponse = getJSON('data/parcoursup-2018-diplomes.json');
+
+var infos = [];
+
 // fetch to getJSON from JS : https://stackoverflow.com/questions/1639555/return-get-data-in-a-function-using-jquery 
 
 function getJSON(file)
@@ -28,14 +35,18 @@ function getJSON(file)
      });
      return result;
 }
+//fetch("fr-esr-parcoursup-2018.json ")
+
 //$.getJSON('parcoursup-2018-diplomes.json', function(data) {
     //console.log('data',data); });
+
 
 // Super important : tout les éléments doivent être inclus dans UN SEUL COMPOSANT
 // Autrement dit, on ne peut pas faire return(<div1></div1> <div2></div2>)
 // Il faut tout intégrer dans "un seul et même bloc" return (<div3>...</div3>)
 
-function Header() { return (<div class=" fixe ">
+function Header() {
+         return (<div class=" fixe ">
         <h1>Orientation ...</h1>
         <h3>L'outil d'orientation fait par les jeunes, pour les jeunes</h3>
         <h3>*Autres = Réorientation (issu d'une autre université) + Etrangés + Reprise d'étude</h3>
@@ -56,7 +67,6 @@ function Menu(){ return( <div><br/>
         </ul>
     </nav><br/><br/><br/><br/></div>)}
     
-var infos = ["DUT","BTS","CPGE"];
 
 function Formations(){
         infos = listeFormations(getJSON('data/parcoursup-2018-diplomes.json'));
@@ -79,15 +89,6 @@ function display(infos) {
         $("#monreact").append(output);
 }
 */
-
-//ReactDOM.render(<Header />, document.getElementById("body"))
-
-ReactDOM.render(<React.Fragment>
-        <Header /><Menu /><Formations /><Diplomes />
-        </React.Fragment>, 
-        document.getElementById("body")
-)
-
         
 
 function listeFormations(table) {
@@ -99,33 +100,66 @@ function listeFormations(table) {
         return maliste;
 }
 
-function Diplomes() {
-        //fetch("fr-esr-parcoursup-2018.json ")
-        var jsonResponse;
-        jsonResponse = getJSON('data/parcoursup-2018-diplomes.json');
-        console.log(jsonResponse);
+function Diplomes() {        
+        // Ajout d'éléments calculés
         jsonResponse.map((table) =>{
                 table["Effectif total des admis"] = table["En Phase Principale__1"] + table["En Phase Complémentaire__1"];
                 table["Autres"] = table["Effectif total des admis"] - table["Dont Bac G__2"] -table["Dont Bac Tech__2"] - table["Dont Bac Pro__2"];      
         })
 
          return(
-         <div>{jsonResponse.map((table) =>(
-                <div class='container'>
+         <div>{jsonResponse.map((table, myId) =>(
+                <div class='container' id={myId}>
                 <li>{table["Effectif total des admis"]} admis</li>
                 <li>Dont {Math.round(1000 * table["Dont Bac G__2"] / table["Effectif total des admis"]) / 10} % venant d'un Bac Général</li>
                 <li>Dont {Math.round(1000 * table["Dont Bac Tech__2"] / table["Effectif total des admis"]) / 10 } % venant d'un Bac Technologique</li>
                 <li>Dont {Math.round(1000 * table["Dont Bac Pro__2"] / table["Effectif total des admis"]) / 10} % venant d'un Bac Professionnel </li>
                 <li>Dont {Math.round(1000 * table["Autres"] / table["Effectif total des admis"]) / 10} % autres (réorientation, étrangés, reprise d'études)</li>
-                </div>))}
-        </div>) 
+                <ChartComponent id={myId} data={table} /> 
+                </div>
+                ))}
+        </div>) // Ne pas oublier de mettre le Diagramme lié à chacune des parties 
 }
 
+// function ChartList(){
+//         return(
+//         <div>{jsonResponse.map((table,id) =>(
+//         <ChartComponent id={id} data={table} />
+//         ))}</div>
+//         )
+// }
+// props permet de récupérer tous les paramètres (attributs) défini dans le composent
+// Exemple ici : props.id et props.data, les arguments/attributs de mes composents #HTML
+function ChartComponent(props){
+        //console.log("props:");
+        //console.log(props);
+        var monId = "pie-chart-" + props.id;
+        return(<div class="chart"> 
+        <canvas id={monId} width="800" height="450">
+        </canvas>
+        </div>)
+}
+
+ReactDOM.render(<React.Fragment>
+        <Header /><Menu /><Formations /><Diplomes />
+        </React.Fragment>, 
+        document.getElementById("body")
+)
+
+var allChart = document.getElementsByClassName("chart")
+
+console.log(allChart);
+console.log(jsonResponse);
+Object.values(allChart).forEach(Chart => {
+                console.log(Chart.children[0].id);
+                console.log(Chart.children[0].id.slice(-1));
+                updateChart(Chart.children[0].id,jsonResponse[Chart.children[0].id.slice(-1)]);
+        });
 
 
-function newChart(id, table) {
+function updateChart(id, table) {
 
-        console.log(id)
+        console.log("id:"+id)
         var keys = ["Dont Bac G__2", "Dont Bac Tech__2", "Dont Bac Pro__2", "Autres"];
         var showKeys = ["Bac Général", "Bac Technologique", "Bac Professionnel", "Autres"];
 
@@ -175,9 +209,12 @@ function newChart(id, table) {
         });
 
 
-        console.log(myChart.data.datasets[0].data);
-        for (j in keys) {
+        console.log(myChart);
+        for (var j in keys) {
         //console.log(myChart.data.datasets[0].data)
+        console.log(j)
+        console.log(table)
+        console.log(table[keys[j]])
         myChart.data.datasets[0].data.push(table[keys[j]]);
         myChart.data.labels.push(showKeys[j]);
         }
@@ -191,3 +228,5 @@ function newChart(id, table) {
         <p>" + value["name"] + " : " + value["count"] + "</p>"); }) } }) //}) }); var nb_msg = 0 //alert(JSON.stringify(jsonResponse.data[4].Periode)); // tableau récupérant tous les nombres de messages //var total = []; //var msg = []; var final_array = [];
         /////////////////////////////////////// var delay = 20; //20 } 
 */
+
+// Appel du render des composants React dans le DOM
