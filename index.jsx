d1894b7d6d2f1928d5843c49bc25353837d81594
Dubuc Valentin <v.dre@laposte.net>
2022-04-17 01:48:13 +0200

    Section Baccalauréat + Etudes Sup

--- index.jsx
+++ index.jsx
@@ -16,6 +16,30 @@ Condition Ternaire : condition ? exprSiVrai : exprSiFaux
 // Déclaration de nos variables/tableaux de données
 var jsonResponse;
 jsonResponse = getJSON('data/parcoursup-2018-diplomes.json');
+var bacData;
+bacData = getJSON('data/parcoursup-2018-baccalaureat.json');
+
+var BacName = ["Bac Général", "Bac Technologique", "Bac Professionnel"];
+
+/*
+// Initialisation des données de chaque baccalauréat
+var bacData = [];
+bacData["Bac G"] = {};
+bacData["Bac Tech"] = {};
+bacData["Bac Pro"] = {};
+
+jsonResponse.map((table) =>{
+        for(var i in bacData){
+                if(bacData[i]["Candidats"] == undefined){
+                        bacData[i]["Candidats"] = {};
+                        bacData[i]["Admis"] = {};
+                }
+                bacData[i]["Candidats"][table['Filière de formation très agrégée']] = table["Dont " + i];
+                bacData[i]["Admis"][table['Filière de formation très agrégée']] = table["Dont " + i + "__2"];
+        }
+})
+console.log(JSON.stringify(bacData))
+*/
 
 var infos = [];
 
@@ -35,6 +59,7 @@ function getJSON(file)
      });
      return result;
 }
+
 //fetch("fr-esr-parcoursup-2018.json ")
 
 //$.getJSON('parcoursup-2018-diplomes.json', function(data) {
@@ -46,8 +71,8 @@ function getJSON(file)
 // Il faut tout intégrer dans "un seul et même bloc" return (<div3>...</div3>)
 
 function Header() {
-         return (<div class=" fixe ">
-        <h1>Orientation ...</h1>
+         return (<div class="title" >
+        <h1 style={{textDecoration:'underline'}} >Orientation ...</h1>
         <h3>L'outil d'orientation fait par les jeunes, pour les jeunes</h3>
         <h3>*Autres = Réorientation (issu d'une autre université) + Etrangés + Reprise d'étude</h3>
     </div>) } 
@@ -67,18 +92,6 @@ function Menu(){ return( <div><br/>
         </ul>
     </nav><br/><br/><br/><br/></div>)}
     
-
-function Formations(){
-        infos = listeFormations(getJSON('data/parcoursup-2018-diplomes.json'));
-        console.log(infos);
-         return(
-        <div class='flexcontainer'>
-        {infos.map((diplome) =>(
-                <div class='diplome' > {diplome}</div>
-        ))}
-        </div>
-)}
-
 /*
 function display(infos) {
         output = "<div class='flexcontainer'>";
@@ -100,7 +113,7 @@ function listeFormations(table) {
         return maliste;
 }
 
-function Diplomes() {        
+function DiplomesAncien() {        
         // Ajout d'éléments calculés
         jsonResponse.map((table) =>{
                 table["Effectif total des admis"] = table["En Phase Principale__1"] + table["En Phase Complémentaire__1"];
@@ -108,119 +121,186 @@ function Diplomes() {
         })
 
          return(
-         <div>{jsonResponse.map((table, myId) =>(
-                <div class='container' id={myId}>
+         <div id='diplomes'>{jsonResponse.map((table, myId) =>(
+                <div class='container' id={"diplomes-"+myId}>
+                <li class='title' >{table["Filière de formation très agrégée"]}</li>
                 <li>{table["Effectif total des admis"]} admis</li>
                 <li>Dont {Math.round(1000 * table["Dont Bac G__2"] / table["Effectif total des admis"]) / 10} % venant d'un Bac Général</li>
                 <li>Dont {Math.round(1000 * table["Dont Bac Tech__2"] / table["Effectif total des admis"]) / 10 } % venant d'un Bac Technologique</li>
                 <li>Dont {Math.round(1000 * table["Dont Bac Pro__2"] / table["Effectif total des admis"]) / 10} % venant d'un Bac Professionnel </li>
                 <li>Dont {Math.round(1000 * table["Autres"] / table["Effectif total des admis"]) / 10} % autres (réorientation, étrangés, reprise d'études)</li>
-                <ChartComponent id={myId} data={table} /> 
+                <ChartComponent id={"diplomes-"+myId} data={table} /> 
                 </div>
                 ))}
         </div>) // Ne pas oublier de mettre le Diagramme lié à chacune des parties 
 }
 
-// function ChartList(){
-//         return(
-//         <div>{jsonResponse.map((table,id) =>(
-//         <ChartComponent id={id} data={table} />
-//         ))}</div>
-//         )
-// }
+
+
+function Diplomes() {        
+
+        var colors = ["#525260","#722030",
+                "#695000","#552055","#4a406f","#026029"]
+
+        // Ajout d'éléments calculés
+        jsonResponse.map((table) =>{
+                table["Effectif total des admis"] = table["En Phase Principale__1"] + table["En Phase Complémentaire__1"];
+                table["Autres"] = table["Effectif total des admis"] - table["Dont Bac G__2"] -table["Dont Bac Tech__2"] - table["Dont Bac Pro__2"];      
+        })
+        
+        var mesDatas = jsonResponse.slice(0,-1); // Sans le "Total"
+         return(
+         <div id='diplomes' class='flexcontainer'>{mesDatas.map((table, myId) =>(
+                <div class='container' id={"diplomes-"+myId} style={{backgroundColor:colors[myId]}}>
+                <li class='title' >{table["Filière de formation très agrégée"]}</li>
+                <li>{table["Effectif total des admis"]} admis</li>
+                <ChartComponent id={"diplomes-"+myId} data={table} size="225" /> 
+                </div>
+                ))}
+        </div>) // Ne pas oublier de mettre le Diagramme lié à chacune des parties 
+}
+
+function Baccalaureat(){
+        var colors = ["#112249","#223369","#335599","#446"];
+
+        return(
+           <div id='bac' class='flexcontainer'>{Object.values(bacData).map((bac,bacIndex) => {
+                return(<div class='container' id={'bac-'+bacIndex} style={{width:'270px',backgroundColor:colors[bacIndex]}}><li class='title' >{ BacName[bacIndex] /*Object.keys(bacData)[bacIndex]*/ }</li>{Object.values(bac['Admis']).map((diplome,diplomeIndex) =>
+                        <li style={{textAlign:'left',marginLeft:'40px'}}>{Object.keys(bac['Admis'])[diplomeIndex] + " : " + diplome}</li>
+                        )}
+                        <ChartComponent id={"bac-"+bacIndex} data={bac} size="280" /> </div>
+                );
+           })}</div>);
+        /* En affichant le nombre de candidats et d'admis
+     return(
+        <div id='bac' class='flexcontainer'>{Object.values(bacData).map((bac,bacIndex) => {
+             return(<div class={'bac-'+bacIndex}>{Object.keys(bacData)[bacIndex]}{Object.values(bac).map((data,dataIndex) =>{
+                     return(<div class='container'>{Object.keys(bac)[dataIndex]}{Object.values(data).map((diplome,diplomeIndex) =>
+                        <li style={{textAlign:'left'}}>{Object.keys(data)[diplomeIndex] + " : " + diplome}</li>
+                        )}
+                        <ChartComponent id={"bac-"+bacIndex} data={bac} /> </div>);
+                })}</div>);
+        })}</div>);
+        */
+}
+
 // props permet de récupérer tous les paramètres (attributs) défini dans le composent
 // Exemple ici : props.id et props.data, les arguments/attributs de mes composents #HTML
 function ChartComponent(props){
         //console.log("props:");
         //console.log(props);
         var monId = "pie-chart-" + props.id;
-        return(<div class="chart"> 
-        <canvas id={monId} width="800" height="450">
+        if(props.size == undefined){
+                props.size = 280;
+        }
+        return(<div class="chart" style={{width:props.size}}> 
+        <canvas id={monId}>
         </canvas>
         </div>)
 }
+//        <DiplomesSup />
 
 ReactDOM.render(<React.Fragment>
-        <Header /><Menu /><Formations /><Diplomes />
+        <Header /><Menu />
+        <h1>Répartition des poursuites d'études</h1>
+        <h1>Répartition des lycéens admis par formation</h1>
+        <Diplomes />
+        <h1>Où vont les lycéens admis ?</h1>
+        <Baccalaureat />
         </React.Fragment>, 
         document.getElementById("body")
 )
 
-var allChart = document.getElementsByClassName("chart")
-
-console.log(allChart);
-console.log(jsonResponse);
-Object.values(allChart).forEach(Chart => {
-                console.log(Chart.children[0].id);
-                console.log(Chart.children[0].id.slice(-1));
+var allDiplomesChart = document.getElementById('diplomes').getElementsByClassName("chart")
+Object.values(allDiplomesChart).forEach(Chart => {
+                //console.log(Chart.children[0].id);
                 updateChart(Chart.children[0].id,jsonResponse[Chart.children[0].id.slice(-1)]);
         });
 
-
-function updateChart(id, table) {
-
-        console.log("id:"+id)
-        var keys = ["Dont Bac G__2", "Dont Bac Tech__2", "Dont Bac Pro__2", "Autres"];
-        var showKeys = ["Bac Général", "Bac Technologique", "Bac Professionnel", "Autres"];
-
-        var data = [{
-        data: [],
-        labels: [],
-        backgroundColor: [
-                "#4b77a9",
-                "#5f255f",
-                "#d21243",
-                "#B27200"
-        ],
-        borderColor: "#fff"
-        }];
-
-        var options = {
-        plugins: {
-                datalabels: {
-                formatter: (value, ctx) => {
-                        let datasets = ctx.chart.data.datasets;
-
-                        if (datasets.indexOf(ctx.dataset) === datasets.length - 1) {
-                        let sum = datasets[0].data.reduce((a, b) => a + b, 0);
-                        let percentage = Math.round((value / sum) * 100) + "%";
-                        if (Math.round((value / sum) * 100) > 5) {
-                                return percentage;
-                        } else {
-                                return "";
-                        }
-                        } else {
-                        return percentage;
-                        }
-                },
-                color: "white"
-                }
-        }
-        };
-
-        var ctx = document.getElementById(id).getContext("2d");
-        var myChart = new Chart(ctx, {
-        type: 'pie',
-        data: {
-                datasets: data
-        },
-        plugins: [ChartDataLabels],
-        options: options
+var allBacChart = document.getElementById('bac').getElementsByClassName("chart")
+Object.values(allBacChart).forEach(Chart => {
+                //console.log(Object.keys(bacData));
+                updateChart(Chart.children[0].id,bacData[Object.keys(bacData)[Chart.children[0].id.slice(-1)]]['Admis']);
         });
 
+function updateChart(id,table){
+        if(id.includes('diplome')){
+                var keys = ["Dont Bac G__2", "Dont Bac Tech__2", "Dont Bac Pro__2", "Autres"];
+                var showKeys = ["Bac Général", "Bac Technologique", "Bac Professionnel","Autres"];
+                var colorsChart = ["#126","#22338f","#335599","#446"];
 
-        console.log(myChart);
-        for (var j in keys) {
-        //console.log(myChart.data.datasets[0].data)
-        console.log(j)
-        console.log(table)
-        console.log(table[keys[j]])
-        myChart.data.datasets[0].data.push(table[keys[j]]);
-        myChart.data.labels.push(showKeys[j]);
+        }else if(id.includes('bac')){
+                //var keys = Object.keys(table).slice(0,-1); // ["Autre formation","BTS","CPGE","DUT","Licence","PACES"];
+                var keys = ["Licence","DUT","BTS","CPGE","PACES","Autre formation"]; 
+                var showKeys = keys;
+                var colorsChart = ["#4b4799","#5f255f","#d21243",
+                        "#B27200","#028250","#927280"];
         }
 
-        myChart.update();
+        var data = [{
+                data: [],
+                labels: [],
+                backgroundColor: [],
+                borderColor: "#fff"
+                }];
+        
+        var options = {
+                plugins: {
+                        datalabels: {
+                        formatter: (value, ctx) => {
+                                let datasets = ctx.chart.data.datasets;
+        
+                                if (datasets.indexOf(ctx.dataset) === datasets.length - 1) {
+                                let sum = datasets[0].data.reduce((a, b) => a + b, 0);
+                                let percentage = Math.round((value / sum) * 100) + "%";
+                                if (Math.round((value / sum) * 100) > 5) {
+                                        return percentage;
+                                } else {
+                                        return "";
+                                }
+                                } else {
+                                return percentage;
+                                }
+                        },
+                        color: "white",
+                        align:'start',
+                        offset:-26
+                        },
+                        legend: {
+                                labels: {
+                                  color: "wheat",  // not 'fontColor:' anymore
+                                  font: {
+                                    size: 14 // 'size' now within object 'font {}'
+                                  }
+                                }
+                        }
+                }
+                };
+        
+                var ctx = document.getElementById(id).getContext("2d");
+                var myChart = new Chart(ctx, {
+                type: 'pie',
+                data: {
+                        datasets: data
+                },
+                plugins: [ChartDataLabels],
+                options: options
+                });
+        
+        for(var j in colorsChart){
+        myChart.data.datasets[0].backgroundColor.push(colorsChart[j]);
+        }
+
+        for (var j in keys) {
+                //console.log(myChart.data.datasets[0].data)
+                //console.log(table[keys[j]])
+                myChart.data.datasets[0].data.push(table[keys[j]]);
+                myChart.data.labels.push(showKeys[j]);
+                }
+        
+                myChart.update();
 }
+
 
 /* function analysis() { } function SourceTable(big_table) { fetch("https://data.enseignementsup-recherche.gouv.fr/api/records/1.0/search/?dataset=fr-esr-parcoursup-2018&q=&lang=FR&rows=10000&facet=session&facet=cod_uai&facet=g_ea_lib_vx&facet=dep_lib&facet=region_etab_aff&facet=acad_mies&facet=fili&facet=form_lib_voe_acc&facet=regr_forma&facet=fil_lib_voe_acc&facet=detail_forma&facet=tri
         ") .then(function(response) { return response.json(); }) .then(function(jsonResponse) { console.log(jsonResponse) Object.values(jsonResponse["facet_groups"][big_table]).forEach(table => { console.log(table) if (Array.isArray(table)) { Object.entries(table).forEach(([key,
